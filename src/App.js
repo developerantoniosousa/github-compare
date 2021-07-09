20116179d40f4179b721afeca0dfb09434221a9d
Antonio Sousa <developerantoniosousa@gmail.com>
2021-07-09 15:49:25 -0300

    Removing components not used from App.js
    Remove Title component never used

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
 
 import GlobalStyle from './styles/global';
 
 import Main from './pages/Main';
-
-const Title = styled.h1`
-  color: #f00;
-  font-size: 32px;
-`;
 
 function App() {
   return (
